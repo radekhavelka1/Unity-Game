dbec6d7cdb9a2e93c7293e182da92fda07196103
NovakDav <NovakDa@post.cz>
2023-05-14 15:59:52 +0200

    Zombicek beha do hajzlu XDDDD
    rip branch

--- Assets/ZombieAI.cs
+++ Assets/ZombieAI.cs
@@ -6,48 +6,55 @@ public class ZombieAI : MonoBehaviour
 {
     public GameObject Target;
     public float speed = 0.5f;
+    public float detectionRadius = 5.0f;
+    public float chaseRadius = 5.0f;
 
     public Transform groundCheck_Zombie;
     public float groundDistance = 0.5f;
     public LayerMask groundMask;
-
-    public float detectionRadius = 5.0f;
     public LayerMask detectionMask;
 
-    bool isGrounded;
-    Vector3 velocity;
-    // Start is called before the first frame update
-    void Start()
+    private bool isGrounded;
+    private Rigidbody rb;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {     
+    private void Update()
+    {
         isGrounded = Physics.CheckSphere(groundCheck_Zombie.position, groundDistance, groundMask);
 
-        if (isGrounded && velocity.y < 0)
+        if (!isGrounded)
         {
-            velocity.y = -2f;
-        }
-
-        /**
-        transform.LookAt(Target.transform);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        Collider[] hits = Physics.OverlapSphere(transform.position, detectionRadius, detectionMask);
-        foreach (Collider collider in hits)
-        {
-            if (collider.gameObject.CompareTag("Player"))
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, Vector3.down, out hit, Mathf.Infinity, groundMask))
             {
-                Target = collider.gameObject;
-                transform.LookAt(Target.transform);
-                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                transform.position = hit.point + Vector3.up * groundDistance;
             }
         }
-        **/
-    }
 
+        float distanceToTarget = Vector3.Distance(transform.position, Target.transform.position);
+        Debug.Log(distanceToTarget);
+        Debug.Log(chaseRadius);
+        if (distanceToTarget <= chaseRadius)
+        {
+            // Player is within chase radius, run towards the player
+            Vector3 direction = (Target.transform.position - transform.position).normalized;
+            rb.velocity = direction * speed;
+            transform.LookAt(Target.transform);
+
+        }
+        else
+        {
+            Vector3 direction = (Target.transform.position - transform.position).normalized;
+            rb.velocity = direction * 0;
+            transform.LookAt(Target.transform);
+        }
+
+
+    }
 
     // Draw the detection radius in the editor
     private void OnDrawGizmosSelected()
